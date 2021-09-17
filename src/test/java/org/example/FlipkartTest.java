b64ff05425f18a197ae8b22406dac8b8f7e73f28
Amaansidd <87928938+Amaansidd@users.noreply.github.com>
2021-09-17 17:49:51 +0530

    y1

--- src/test/java/org/example/FlipkartTest.java
+++ src/test/java/org/example/FlipkartTest.java
@@ -52,8 +52,8 @@ public class FlipkartTest {
 
     @BeforeTest
     public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver","F:\\\\chrome-driver\\chromedriver.exe");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
     }
 
     @AfterTest
