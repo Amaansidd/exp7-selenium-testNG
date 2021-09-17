1c191fb8d90c10a2c5610c514638358b76774815
Amaansidd <87928938+Amaansidd@users.noreply.github.com>
2021-09-17 17:50:23 +0530

    y2

--- src/test/java/org/example/W3schoolsTest.java
+++ src/test/java/org/example/W3schoolsTest.java
@@ -26,8 +26,8 @@ public class W3schoolsTest {
 
     @BeforeTest
     public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver","F:\\\\chrome-driver\\chromedriver.exe");
-        driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
     }
 
     @AfterTest
